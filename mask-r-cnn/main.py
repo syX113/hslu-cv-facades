1190359acf87f9c2e6a2dca315ea8e163e446683
Tim Giger <tim.giger93@gmail.com>
2023-10-08 13:38:46 +0200

    added hp tuning for Mask-R-CNN

--- mask-r-cnn/main.py
+++ mask-r-cnn/main.py
@@ -6,11 +6,72 @@ from detectron2.engine import DefaultPredictor, hooks
 from trainer import CustomDetectronTrainer
 from model_config import get_mask_config
 from utils.training_utils import init_gpu, log_predictions
+from detectron2.data import build_detection_test_loader
+from detectron2.evaluation import COCOEvaluator, inference_on_dataset
+import optuna
+
+# Define the Objective function for Optuna
+def objective(trial):
+    
+    # Define the hyperparameter search space
+    lr = trial.suggest_float("lr", 0.0001, 0.01, log=True)
+    step1 = trial.suggest_int("step1", 500, 5000, step=500)
+    step2 = trial.suggest_int("step2", step1+500, 10000, step=500)
+    max_iter = trial.suggest_int("max_iter", 1000, 10000, step=1000)
+    weight_decay = trial.suggest_float("weight_decay", 1e-5, 1e-3, log=True)
+    momentum = trial.suggest_float("momentum", 0.85, 0.99)
+    batch_size_per_image = trial.suggest_categorical("batch_size_per_image", [64, 128, 256, 512])
+    anchor_sizes = trial.suggest_categorical("anchor_sizes", [[32, 64, 128], [64, 128, 256], [128, 256, 512]])
+    warmup_factor = trial.suggest_float("warmup_factor", 0.01, 1)
+    warmup_iters = trial.suggest_int("warmup_iters", 100, 1000, step=100)
+    batch_size = trial.suggest_categorical("batch_size", [2, 4, 8])
+
+    cfg = get_mask_config()
+    
+    # Overwriting default model config
+    cfg.SOLVER.BASE_LR = lr
+    cfg.SOLVER.IMS_PER_BATCH = batch_size
+    cfg.SOLVER.STEPS = [step1, step2]
+    cfg.SOLVER.MAX_ITER = max_iter
+    cfg.SOLVER.WEIGHT_DECAY = weight_decay
+    cfg.SOLVER.MOMENTUM = momentum
+    cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = batch_size_per_image
+    cfg.MODEL.ANCHOR_GENERATOR.SIZES = [anchor_sizes]
+    cfg.SOLVER.WARMUP_FACTOR = warmup_factor
+    cfg.SOLVER.WARMUP_ITERS = warmup_iters
+
+    experiment = create_experiment()
+    experiment.log_parameters(trial.params)
+
+    trainer = CustomDetectronTrainer(cfg, experiment)
+    setup_hooks(trainer, cfg)
+
+    trainer.train()
+    
+    # Using AP metric as objective for optimization. 
+    segm_ap = evaluate_model(cfg, experiment)
+    experiment.log_metric("validation_segm_ap", segm_ap)
+    
+    return segm_ap
+
+
+def hp_tuning_train_mask():
+    
+    study = optuna.create_study(direction="maximize") # Using "maximize", since AP metric should be maximized  
+    study.optimize(objective, n_trials=25)
+
+    print("Best trial:")
+    trial = study.best_trial
+
+    print("Value: ", trial.value)
+    print("Params: ")
+    for key, value in trial.params.items():
+        print(f"    {key}: {value}")
 
 def train_mask():
        
     # Create the configuration and Comet experiment
-    #cfg = get_mask_config(default_augs=False, custom_augs=None, sampling=False)
+    # cfg = get_mask_config(default_augs=False, custom_augs=None, sampling=False)
     cfg = get_mask_config(custom_augs=["CustomAugmentationGreyscale", "CustomAugmentationCLAHE"])
     
     experiment = create_experiment()
@@ -48,10 +109,20 @@ def evaluate_model(cfg, experiment):
     predictor = DefaultPredictor(cfg)
     log_predictions(predictor, experiment, 'facade_test_city')
     
+    # Evaluate on the validation set
+    eval_output_dir = os.path.join(cfg.OUTPUT_DIR, "evaluation")
+    os.makedirs(eval_output_dir, exist_ok=True) 
+    evaluator = COCOEvaluator("facade_valid_city", cfg, False, output_dir=eval_output_dir)
+    val_loader = build_detection_test_loader(cfg, "facade_valid_city")
+    results = inference_on_dataset(predictor.model, val_loader, evaluator)
+    
+    return results["segm/AP"]
 
 if __name__ == "__main__":
     
     # Clear GPU cache and get CUDA/Torch versions
     init_gpu()
     # Start the training loop
-    train_mask()
+    #train_mask()
+    # Start the hyperparameter search training loop
+    hp_tuning_train_mask()
