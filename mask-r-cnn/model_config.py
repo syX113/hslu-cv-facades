c8ae2553d7a352bb0fddb7cb2598e2f8a804a1e9
Tim Giger <tim.giger93@gmail.com>
2023-10-09 12:58:43 +0200

    added nodes to CFG

--- mask-r-cnn/model_config.py
+++ mask-r-cnn/model_config.py
@@ -19,24 +19,32 @@ def get_mask_config(output_directory='./mask-r-cnn/_output/', default_augs=False
     # Define model configuration
     # Reference CFG parameters: https://detectron2.readthedocs.io/en/latest/modules/config.html#yaml-config-references
     cfg = get_cfg()
-    cfg.MODEL.DEVICE = "cuda:2" # Set GPU or CPU devices, e.g. "cuda:3" to use specific GPU
+    cfg.MODEL.DEVICE = "cuda:3" # Set GPU or CPU devices, e.g. "cuda:3" to use specific GPU
     cfg.OUTPUT_DIR = output_directory
     cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_X_101_32x8d_FPN_3x.yaml"))
     cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_X_101_32x8d_FPN_3x.yaml")
     cfg.DATASETS.TRAIN = ("facade_train_city",)
     cfg.DATASETS.TEST = ("facade_valid_city",)
+    cfg.INPUT.MASK_FORMAT = "polygon"
     cfg.DATALOADER.NUM_WORKERS = 8
+    cfg.INPUT.MAX_SIZE_TRAIN = 1024
+    cfg.INPUT.MAX_SIZE_TEST = 1024
+    cfg.INPUT.MIN_SIZE_TEST = 0 # Disable resizing, therefore value "0"
     cfg.SOLVER.IMS_PER_BATCH = 4
     cfg.SOLVER.BASE_LR = 0.0025
     cfg.SOLVER.MAX_ITER = 3000
+    cfg.INPUT.RANDOM_FLIP = 'none'
+    cfg.INPUT.CROP.ENABLED = False
     cfg.SOLVER.STEPS = [] # Learning rate decay
     cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 256
     cfg.MODEL.ROI_HEADS.NUM_CLASSES = 10 + 1  # Number of classes + background
     
     # Turn off default augmentations
     if default_augs:
-        cfg.INPUT.RANDOM_FLIP = 'none'
-        cfg.INPUT.CROP.ENABLED = False
+        cfg.INPUT.RANDOM_FLIP = 'horizontal'
+        cfg.INPUT.CROP.ENABLED = True
+        cfg.INPUT.CROP.TYPE = "relative_range"
+        cfg.CROP.SIZE = [0.7, 0.7]
         
     if sampling:
         repeat_factors = class_sampling('facade_train_city')
@@ -46,11 +54,10 @@ def get_mask_config(output_directory='./mask-r-cnn/_output/', default_augs=False
         
     # Initialize AUGMENTATION.CUSTOM_LIST to add the custom augmentations to the config
     cfg.AUGMENTATION = CN()
-    
     if custom_augs is None:
         custom_augs = []
-        
-    cfg.AUGMENTATION.CUSTOM_LIST = custom_augs
+    else:    
+        cfg.AUGMENTATION.CUSTOM_LIST = custom_augs
     
     # Create the output directory if not exists
     os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
